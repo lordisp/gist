89329851780799147ce10ead4e4a457ee87e877b
Rafael Camison <rafael.camison@outlook.com>
2021-03-17 19:55:38 +0100

    show or export vnet-report based on tenant-selection

--- get_vnet_report.ps1
+++ get_vnet_report.ps1
@@ -1,11 +1,24 @@
+[CmdletBinding()]
+param (
+    [parameter(Mandatory = $true)]
+    $TenantId,
+    [parameter(Mandatory = $false)]
+    $Subscription,
+    [Switch]$Export
+)
 $context = $null
 $context = Get-AzContext
+$tenantName = (Get-AzTenant -TenantId $TenantId).name
+$fileName = $((Get-Date).ToString('yyyy-MM-dd-hh-mm')) + "-vNetReport_$tenantName.csv"
+if ($context.Tenant -ne $TenantId) {
+    $context = Connect-AzAccount -Tenant $TenantId -WarningAction SilentlyContinue
+}
 if ($context) {
     Write-Host "You're' logged in" -ForegroundColor Green
 }
 else {
-    Write-Host "You're not logged in to Azure" -ForegroundColor Red
-    $context = Connect-AzAccount 
+    Write-Host "You're not logged in to Azure" -ForegroundColor Yellow
+    $context = Connect-AzAccount -Tenant $TenantId
 }
 function Get-AzRestVirtualNetwork {
     [CmdletBinding()]
@@ -19,18 +32,33 @@ function Get-AzRestVirtualNetwork {
         'Content-Type'  = 'application/json';
         'Authorization' = "Bearer" + " " + "$($Token.Token)";
     }
-    $Query = Invoke-RestMethod -Method Get -Uri $ApiUri -Headers $headers
+    try {
+        $Query = Invoke-RestMethod -Method Get -Uri $ApiUri -Headers $headers -ErrorAction SilentlyContinue
+    }
+    catch {
+        Write-Error "Error with Subscription $($SubscriptionId)"
+        Write-Error $_
+    }
+    
     ForEach ($Result in $Query.value) {
         [PSCustomObject]@{
             vNetName        = $Result.Name
-            <# vNetId          = $Result.id #>
             addressPrefixes = $Result.properties.addressSpace.addressPrefixes
-            subnets         = $Result.properties.subnets.name 
-            addressPrefix   = $Result.properties.subnets.properties.addressPrefix
         }
     } 
 
-    <# return $Query.value | select -First 1 #>
+    return $Query.value 
 }
-#$Subscriptions = Get-AzSubscription 
-Get-AzRestVirtualNetwork -SubscriptionId '18d6c26e-6e4c-4d49-9849-e8d15fb21b08'
+$Subscriptions = Get-AzSubscription -SubscriptionName $Subscription -TenantId $TenantId | Where-Object { $_.State -eq 'Enabled' -and $_.name -ne 'Zugriff auf Azure Active Directory' -and $_.name -ne 'Access to Azure Active Directory' }
+
+foreach ($Subscription in $Subscriptions) {
+    $AzRestVirtualNetwork = Get-AzRestVirtualNetwork -SubscriptionId $Subscription.Id | Select-Object @{n = 'Subscription'; e = { $Subscription.Name } }, `
+        vNetName, `
+    @{n = 'addressPref1'; e = { $_.addressPrefixes[0] } }, `
+    @{n = 'addressPref2'; e = { $_.addressPrefixes[1] } } | `
+        Where-Object { $_.vNetName.Length -gt 0 }
+    if ($Export) {
+        $AzRestVirtualNetwork | Export-CSV ".\$fileName" -Delimiter ';' -force -notypeinformation -Append
+    }
+    $AzRestVirtualNetwork
+}
