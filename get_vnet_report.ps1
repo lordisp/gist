f7ccab8f0c741722d8d65f735bceab6d2f3a4b84
Rafael Camison <rafael.camison@outlook.com>
2021-03-17 22:07:27 +0100

    add feature 'html report'

--- get_vnet_report.ps1
+++ get_vnet_report.ps1
@@ -4,12 +4,13 @@ param (
     $TenantId,
     [parameter(Mandatory = $false)]
     $Subscription,
-    [Switch]$Export
+    [Switch]$Export,
+    [Switch]$html
 )
 $context = $null
 $context = Get-AzContext
 $tenantName = (Get-AzTenant -TenantId $TenantId).name
-$fileName = $((Get-Date).ToString('yyyy-MM-dd-hh-mm')) + "-vNetReport_$tenantName.csv"
+$fileName = $((Get-Date).ToString('yyyy-MM-dd-hh-mm')) + "-vNetReport_$tenantName"
 
 if ($context) {
     Write-Host "You're logged in to Tenant $([char]034)$($tenantName)$([char]034) as $($context.Account.id)" -ForegroundColor Green
@@ -40,26 +41,28 @@ function Get-AzRestVirtualNetwork {
         Write-Error "Error with Subscription $($SubscriptionId)"
         Write-Error $_
     }
-    
-    ForEach ($Result in $Query.value) {
-        [PSCustomObject]@{
-            vNetName        = $Result.Name
-            addressPrefixes = $Result.properties.addressSpace.addressPrefixes
-        }
-    } 
-
     return $Query.value 
 }
 $Subscriptions = Get-AzSubscription -SubscriptionName $Subscription -TenantId $TenantId | Where-Object { $_.State -eq 'Enabled' -and $_.name -ne 'Zugriff auf Azure Active Directory' -and $_.name -ne 'Access to Azure Active Directory' }
-
+$AzRestVirtualNetwork = @()
 foreach ($Subscription in $Subscriptions) {
-    $AzRestVirtualNetwork = Get-AzRestVirtualNetwork -SubscriptionId $Subscription.Id | Select-Object @{n = 'Subscription'; e = { $Subscription.Name } }, `
-        vNetName, `
-    @{n = 'addressPref1'; e = { $_.addressPrefixes[0] } }, `
-    @{n = 'addressPref2'; e = { $_.addressPrefixes[1] } } | `
-        Where-Object { $_.vNetName.Length -gt 0 }
+    $Objects = Get-AzRestVirtualNetwork -SubscriptionId $Subscription.Id 
+    foreach ($Object in $Objects) {
+        $Current = [ordered]@{'Subscription' = $Subscription.Name; 'vNetName' = $Object.Name; 'addressPref1' = $Object.properties.addressSpace.addressPrefixes[0]; 'addressPref2' = $Object.properties.addressSpace.addressPrefixes[1]; }
+        $AzRestVirtualNetwork += New-Object PSObject -Property $Current
+    }
     if ($Export) {
-        $AzRestVirtualNetwork | Export-CSV ".\$fileName" -Delimiter ';' -force -notypeinformation -Append
+        if ($html) {
+            $a = "<style>"
+            $a = $a + "TABLE{border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;font-family:arial}"
+            $a = $a + "TH{border-width: 1px;padding: 5px;border-style: solid;border-color: black;}"
+            $a = $a + "TD{border-width: 1px;padding: 5px;border-style: solid;border-color: black;}"
+            $a = $a + "</style>"
+            $AzRestVirtualNetwork  | ConvertTo-Html -Head $a | Out-file ".\$fileName.html"
+        }
+        else {
+            $AzRestVirtualNetwork | Export-CSV ".\$fileName.csv" -Delimiter ';' -force -notypeinformation -Append
+        }
     }
     $AzRestVirtualNetwork
 }
