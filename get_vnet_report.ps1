0ade48d6f0cdf49e7280ddffb82165143d8ce53e
Rafael Camison <rafael.camison@outlook.com>
2021-03-19 18:04:22 +0100

    update progress-bar and fix open export-file after completion

--- get_vnet_report.ps1
+++ get_vnet_report.ps1
@@ -5,6 +5,11 @@ This Stript reports or exports Virtual Network Details in all or just a single S
 Determines which Azure Tenant shall be reported
 .PARAMETER Subscription
 To limit the report down to just a single Subscription.
+.INPUTS
+System.String
+System.Boolean
+.OUTPUTS
+System.Management.Automation.PSCustomObject
 .EXAMPLE
 get_vnet_report.ps1 -TenantId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx -Export
 Export all Virtual Networks from all Subscriptions from the TenantId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx to a CSV File
@@ -13,7 +18,18 @@ get_vnet_report.ps1 -TenantId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx -Export -Html
 Export all Virtual Networks from all Subscriptions from the TenantId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx to a Html File
 .EXAMPLE
 get_vnet_report.ps1 -TenantId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
-Prints all Virtual Networks from all Subscriptions from the TenantId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx to the current Terminal
+Prints all Virtual Networks from all Subscriptions:
+Subscription            vNetName    addressPref1    addressPref2    CostCenter  PspElement
+------------            --------    ------------    ------------    ----------  -----------
+SUBSCRIPTION_NAME_123   vnet-123    10.0.0.0/24     192.10.10.0/24  123456789   X/YYY-ZZZ-000.01
+SUBSCRIPTION_NAME_456   vnet-123    10.0.0.1/24     192.10.20.0/24  9879654321  X/YYY-ZZZ-000.02
+SUBSCRIPTION_NAME_789   vnet-123    10.0.0.2/24     192.10.30.0/24  123987456   X/YYY-ZZZ-000.03
+.EXAMPLE
+get_vnet_report.ps1 -TenantId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx -Subscription SUBSCRIPTION_NAME_123
+Prints all Virtual Networks from a single Subscriptions:
+Subscription            vNetName    addressPref1    addressPref2    CostCenter  PspElement
+------------            --------    ------------    ------------    ----------  -----------
+SUBSCRIPTION_NAME_123   vnet-123    10.0.0.0/24     192.10.10.0/24  123456789   X/YYY-ZZZ-000.01
 .EXAMPLE
 get_vnet_report.ps1 -TenantId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx -Subscription SUBSCRIPTION_NAME_123 -Export
 Export all Virtual Networks from a single Subscription from the TenantId xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx to a CSV File
@@ -24,6 +40,15 @@ Export all Virtual Networks from a single Subscription from the TenantId xxxxxxx
     Author: Rafael Camison
     Date:   Mar 17th, 2021
 Check out https://github.com/lordisp/gist/blob/main/get_vnet_report.ps1 for the latest updates on this script
+
+.LINK
+https://github.com/lordisp/gist/issues
+
+.LINK
+https://docs.microsoft.com/powershell/azure/install-az-ps
+
+.LINK
+https://docs.microsoft.com/en-us/rest/api/virtualnetwork/virtualnetworks/listall
 #>
 [CmdletBinding()]
 param (
@@ -71,25 +96,18 @@ function Get-AzRestVirtualNetwork {
     return $Query.value 
 }
 $Subscriptions = Get-AzSubscription -SubscriptionName $Subscription -TenantId $TenantId | Where-Object { $_.State -eq 'Enabled' -and $_.name -ne 'Zugriff auf Azure Active Directory' -and $_.name -ne 'Access to Azure Active Directory' }
-$i = 1
+$i = 0
 $AzRestVirtualNetwork = @()
 Clear-Host
 foreach ($Subscription in $Subscriptions) {
-    if ($Export) {
-        Write-Progress -Activity "Fetching vNet Information from $($Subscription.Name)" -Status "Subsctiption $i out of $($Subscriptions.count)" -PercentComplete (($i / $Subscriptions.Count) * 100)  
-        $i++
-        if ($i -eq $Subscriptions.count) {
-            Write-Host "Export Completed!" -ForegroundColor Green
-            $filepath = Get-ChildItem $fileName*
-            Write-Host "$($pwd)\$($filepath.Name)" -ForegroundColor Yellow
-        }
-    }
     $Objects = Get-AzRestVirtualNetwork -SubscriptionId $Subscription.Id 
     foreach ($Object in $Objects) {
         $Current = [ordered]@{'Subscription' = $Subscription.Name; 'vNetName' = $Object.Name; 'addressPref1' = $Object.properties.addressSpace.addressPrefixes[0]; 'addressPref2' = $Object.properties.addressSpace.addressPrefixes[1]; 'CostCenter' = $Object.tags.cost_center; 'PspElement' = $Object.tags.psp_element; }
         $AzRestVirtualNetwork += New-Object PSObject -Property $Current
     }
     if ($Export) {
+        Write-Progress -Activity "Fetching vNet Information from $($Subscription.Name)" -Status "Subsctiption $i out of $($Subscriptions.count)" -PercentComplete (($i / $Subscriptions.Count) * 100)  
+        $i++
         if ($Html) {
             $a = "<style>"
             $a = $a + "TABLE{border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;font-family:arial}"
@@ -100,6 +118,12 @@ foreach ($Subscription in $Subscriptions) {
         }
         else {
             $AzRestVirtualNetwork | Export-CSV ".\$fileName.csv" -Delimiter ';' -force -notypeinformation -Append
+        }
+        if ($i -eq $Subscriptions.count) {
+            Write-Host "Export Completed!" -ForegroundColor Green
+            $filepath = Get-ChildItem "$($fileName)*" -File
+            Write-Host "$($pwd)\$($filepath.Name)" -ForegroundColor Yellow
+            & ".\$($filepath.Name)"
         }
     }
     if (!$export) {
